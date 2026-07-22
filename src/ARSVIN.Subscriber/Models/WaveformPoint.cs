e13d0db24b135c19b6884645d8fafb14307c443b
masarray <ari.sulistiono@gmail.com>
2026-07-22 14:23:53 +0700

    Carry waveform unit and scaling provenance

--- src/ARSVIN.Subscriber/Models/WaveformPoint.cs
+++ src/ARSVIN.Subscriber/Models/WaveformPoint.cs
@@ -4,6 +4,9 @@ public sealed class WaveformPoint
 {
     public int Index { get; init; }
     public ushort? SampleCount { get; init; }
+    public string CurrentUnit { get; init; } = "count";
+    public string VoltageUnit { get; init; } = "count";
+    public string ScalingSummary { get; init; } = "Raw counts";
     public double? Ia { get; set; }
     public double? Ib { get; set; }
     public double? Ic { get; set; }
